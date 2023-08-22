97be75a40025f5b0497c30c4aee8ac1e44d7ac8b
root <idrys01@gmail.com>
2023-08-22 02:38:34 -0700

    preparation of task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from dotenv import load_doten
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+
+load_dotenv()
+is_file = os.getenv("HBNB_TYPE_STORAGE") == 'FileStorage'
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+
+    if (!is_file):
+        cities = relationship
+        'City', cascade='all, delete-orphan',
+        backref='state')
+    else:
+        from models import storage
+        @property
+        def cities(self):
+            return  list(
+                    item['id'] for item in storage.all().values()
+                    if item['__class__'] == 'City'
+                    and item['state_id'] == self.id)
