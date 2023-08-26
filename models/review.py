6be54fd2a342e97fc432383b01a07c3756763d0d
Saysherief <suhairdama@gmail.com>
2023-08-26 16:25:50 -0700

    DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -15,9 +15,7 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-
 else:
-
     class Review(BaseModel):
         """ Review class to store review information in file storage"""
         place_id = ""
