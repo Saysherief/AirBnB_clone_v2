422ca2e7b4f5b734ab1e484334871051a58dd560
Saysherief <suhairdama@gmail.com>
2023-08-26 14:39:28 -0700

    DBStorage - Place with City & User relationship

--- models/city.py
+++ models/city.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import Column, String, ForeignKey
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 
+from os import getenv
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    import sqlalchemy
+    from sqlalchemy import Column, String, ForeignKey
+    from models.base_model import Base
+    from sqlalchemy.orm import relationship
 
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    name = Column(String(128), nullable=False)
+    class City(BaseModel, Base):
+        """ The city class, contains state ID and name """
+        __tablename__ = 'cities'
+
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete-orphan")
+
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+
+else:
+    class City(BaseModel):
+        """ The city class, contains state ID and name """
+        state_id = ""
+        name = ""
