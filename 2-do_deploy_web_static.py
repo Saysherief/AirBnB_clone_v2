f7fabe75f7cf526518c30df2a9e828af52dbd840
Idris Adebowale <idrys01@gmail.com>
2023-09-07 08:49:19 -0700

    add script to deploy release

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""A fabric configuration module to manage static web deployment
+
+This module contain
+    function:
+    do_pack: that bundles and compresses all content of the
+            web_static to  version folder
+    do_deploy: that takes a parameter 'archive_path' which is the
+            location of the archive file
+"""
+import os
+import fabric.api as api
+from datetime import datetime
+from pathlib import Path
+
+api.env.hosts = ['54.157.170.157', '100.25.181.11']
+
+
+def do_pack():
+    """Bundles convert the contents of web_static directory to tgz
+    """
+    version_dir = Path('./versions')
+    if not version_dir.exists():
+        os.mkdir(version_dir)
+    now = datetime.now()
+
+    # absolute path to the compressed file
+    file_name = version_dir / "web_static_{}{}{}{}{}{}.tgz".format(
+            now.year, now.month, now.day,
+            now.hour, now.minute, now.second)
+    try:
+        api.local(f"tar -zcvf {file_name.absolute()} web_static")
+        return file_name.absolute()
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """Deploys archive path to production
+    """
+    if not Path(archive_path).exists():
+        return False
+    try:
+        file_name = archive_path.split('/')[-1]
+        file_name_no_ext = file_name.split('.')[-2]
+        old_path = '/data/web_static/releases/{}/web_static'.format(
+                file_name_no_ext)
+        new_path = '/data/web_static/releases/{}'.format(
+                file_name_no_ext)
+        curr_path = '/data/web_static/current'
+
+        api.put(archive_path, '/tmp/')
+        api.run(f'mkdir -p {new_path}')
+        api.run(f'tar -zxf /tmp/{file_name} -C {new_path}')
+        api.run(f'rm /tmp/{file_name}')
+        api.run(f'rm -rf {curr_path}')
+        api.run(f'mv {old_path}/* {new_path}')
+        api.run(f'rm -rf {old_path}')
+        api.run(f'ln -s {new_path} {curr_path}')
+        return True
+    except Exception as e:
+        print(e)
+        return False
