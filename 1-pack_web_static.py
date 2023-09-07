6704727700cc85bfd93fa5f55e53e5e6299ff870
Idris Adebowale <idrys01@gmail.com>
2023-09-07 02:01:26 -0700

    add script the compress compress a folder

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A fabric configuration module to manage static web deployment
+
+This module contain a function do_pack that bundles and compresses
+all content of the web_static to  version folder
+"""
+import os
+import fabric.api as api
+from datetime import datetime
+from pathlib import Path
+
+
+def do_pack():
+    """Bundles convert the contents of web_static directory to tgz
+    """
+    version_dir = Path('./version')
+    if not version_dir.exists():
+        os.mkdir(version_dir)
+    now = datetime.now()
+
+    # absolute path to the compressed file
+    file_name = version_dir / "web_static_{}{}{}{}{}{}.tgz".format(
+            now.year, now.month, now.day,
+            now.hour, now.minute, now.second)
+    try:
+        api.local(f"tar -zcvf {file_name.absolute()} web_static")
+        return file_name.absolute()
+    except Exception:
+        return None
