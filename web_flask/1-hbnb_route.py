e37109133d8f381e7079725b618b01f9e16f82f6
Saysherief <suhairdama@gmail.com>
2023-09-24 15:50:53 -0700

    Flask web application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,8 +7,15 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """ Function that routes '/': display 'Hello HBNB!'"""
+    """ Function that routes '/':display 'Hello HBNB!'
+    """
     return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """ Function that routes '/hbnb':display 'HBNB'"""
+    return "HBNB"
 
 
 if __name__ == '__main__':
